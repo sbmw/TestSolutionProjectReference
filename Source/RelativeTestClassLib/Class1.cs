cdcc76fdb4d1be8803d9983760458c1ebc6c081f
Matthew Worley <matt@startingblock.digital>
2023-07-13 09:15:32 +0100

    Add code

---
+++ Source/RelativeTestClassLib/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace RelativeTestClassLib;
+
+public class Class1
+{
+    public void Test1()
+    {
+        int val = 0;
+        Console.WriteLine(val);
+    }
+
+    public void Test2()
+    {
+        int val = 0;
+        Console.WriteLine(val);
+    }
+
+    public string? SomeValue { get; set; }
+}
---
+++ Source/TestConsole/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+var x = new RelativeTestClassLib.Class1();
+Console.WriteLine(x);
+
+var y = new FancyTestClassLib.Class1();
+Console.WriteLine(y);
